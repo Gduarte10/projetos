065712132fac44b753c869f9b31210829bdfb187
Gabriel duarte <duartegabriel0102@gmail.com>
2024-06-07 21:22:36 -0300

    criação de aquivos de exmplos

---
+++ exmplo1.py
@@ -0,0 +1,17 @@
+print("hello, word")
+
+print(5+2)
+
+print(6-4)
+
+print(5*4)
+
+print(10/4)
+
+print(10//4)
+
+print(5%2)
+
+print(2**3)
+
+print(5+2+6-4-5*4*10/4/10//4//5%2%2**3)
---
+++ exmplo2.py
@@ -0,0 +1,9 @@
+'''
+Data: 07/06/2024
+desenvolvido por: Gabriel 
+'''
+#variavel
+nota1 = float(input('Digite sua primeira nota.'))
+nota2 = float(input('Digite sua segunda nota.'))
+media = (nota1+nota2)/2
+print('Média:',media)
